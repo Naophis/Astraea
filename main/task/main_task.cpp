8ebab05f4e0003c200e99554c7fa62dbf088b811
Naophis <nao1288stusj@gmail.com>
2025-10-16 00:54:28 +0900

    fix: reset target and ego data in test_run and exec_path_running methods
    Signed-off-by: Naophis <nao1288stusj@gmail.com>

--- main/task/main_task.cpp
+++ main/task/main_task.cpp
@@ -2237,7 +2237,9 @@ void MainTask::test_system_identification(bool para) {
 
 void MainTask::test_run() {
   mp->reset_gyro_ref_with_check();
-
+  reset_tgt_data();
+  reset_ego_data();
+  pt->motor_enable();
   if (sys.test.suction_active == 1) {
     pt->suction_enable(sys.test.suction_duty, sys.test.suction_duty_low);
     vTaskDelay(xDelay500);
@@ -2373,6 +2375,9 @@ void MainTask::test_back() {
 void MainTask::test_run_sla() {
   mp->reset_gyro_ref_with_check();
 
+  reset_tgt_data();
+  reset_ego_data();
+  pt->motor_enable();
   if (sys.test.suction_active == 1) {
     pt->suction_enable(sys.test.suction_duty, sys.test.suction_duty_low);
     vTaskDelay(xDelay500);
